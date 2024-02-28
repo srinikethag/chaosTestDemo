636c7304bf8767f800d31824b1a358a29449f617
Sriniketha Geejagar <sriniketha.geejagar@toggl.com>
2024-02-28 09:34:59 +0530

    working changes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// Make AJAX request to backend Lambda function
-fetch('https://all2i218t8.execute-api.us-east-1.amazonaws.com/dev/hello')
-  .then(response => response.json())
-  .then(data => {
-    // Update UI with response data
-    document.getElementById('message').innerText = data.message;
-  })
-  .catch(error => console.error('Error fetching data:', error));
+document.addEventListener("DOMContentLoaded", function() {
+    // Event listener for "Query" button
+    document.getElementById("queryBtn").addEventListener("click", function() {
+      invokeLambda();
+    });
+  });
+  
+  function invokeLambda() {
+    fetch('https://all2i218t8.execute-api.us-east-1.amazonaws.com/dev/hello')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => console.error('Error:', error));
+  }
--- chaos-lambda/handler.js
+++ chaos-lambda/handler.js
@@ -1,17 +1,32 @@
-'use strict';
-const fs = require('fs');
-
-// Read the HTML file
-const htmlContent = fs.readFileSync('app.html', 'utf8');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const failureLambda = require('failure-lambda');
 
 exports.hello = failureLambda(async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html',
-    },
-    body: htmlContent
-  };
+
+  // Business Logic Here wrapped inside failureLambda
+  try {
+    const params = {
+      Bucket: 'ui-bucket-demo-chaos',
+      Key: 'app.html'
+    };
+
+    const data = await s3.getObject(params).promise();
+    const htmlContent = data.Body.toString('utf-8');
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: htmlContent
+    };
+  } catch (error) {
+    console.error('Error fetching HTML file from S3:', error);
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error'
+    };
+  }
 });
 
